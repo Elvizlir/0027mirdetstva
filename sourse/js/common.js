efd353054fc8dd2dc41e609bf663c58a44264183
Roman Makeev <elvizlir@gmail.com>
2019-12-24 14:22:03 +0300

    task changes

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -200,8 +200,8 @@ function eventHandler() {
 	});
 
 	const icon = '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve" ><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z" ></path>';
-	const arrl2 = (' <div class="r">' + icon),
-		arrr2 = (' <div class="l">' + icon);
+	const arrl = (' <div class="r">' + icon),
+		arrr = (' <div class="l">' + icon);
 	// // карусель
 
 	const defaultSlide = {
@@ -209,30 +209,22 @@ function eventHandler() {
 		infinite: true,
 		arrows: true,
 		mobileFirst: true,
-		prevArrow: arrr2,
-		nextArrow: arrl2,
+		prevArrow: arrr,
+		nextArrow: arrl,
 		// autoplay: true,
 		autoplaySpeed: 6000,
 		lazyLoad: 'progressive',
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
 	};
+
 	$('.s-main-slider__slider--js').slick({
 		...defaultSlide,
-
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: true,
-		appendDots: $('.s-main-slider__slider-dots'),
-
 	});
-	$('.s-new__slider--js').slick({
-		...defaultSlide,
 
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: true,
-		appendDots: $('.s-new__slider-dots'),
-		// prevArrow: $('.s-main-slider__slider-btn--left'),
-		// nextArrow: $('.s-main-slider__slider-btn--right'),
+	$('.s-catalog-slider__slider--js').slick({
+		...defaultSlide,
 
 		responsive: [
 			{
@@ -264,73 +256,7 @@ function eventHandler() {
 		]
 
 	});
-	$('.s-recomend__slider--js').slick({
-		...defaultSlide,
-
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: true,
-		appendDots: $('.s-recomend__slider-dots'),
-		// prevArrow: $('.s-main-slider__slider-btn--left'),
-		// nextArrow: $('.s-main-slider__slider-btn--right'),
-
-		responsive: [
-			{
-				breakpoint: 990,
-				settings: {
-					slidesToShow: 4,
-					slidesToScroll: 1,
-					infinite: true,
-					dots: true
-				}
-			},
-			{
-				breakpoint: 770,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 1
-				}
-			}
-			// You can unslick at a given breakpoint now by adding:
-			// settings: "unslick"
-			// instead of a settings object
-		]
-
-	});
-	// $('.s-gal__slider\
-	// ,.slider-for2 ')
-	// 	.on('lazyLoaded', function (event, slick, image, imageSource) {
-	// 		image.parent().css('background-image', 'url(' + image.attr('src') + ')');
-	// 	});
-	// slider
-	// const swiper4 = new Swiper('.color-slider', {
-	// 	// slidesPerView: 5,
-	// 	slidesPerView: 'auto',
-	// 	watchOverflow: true,
-	// 	spaceBetween: 0,
-	// 	freeMode: true,
-	// 	watchOverflow: true,
-	// 	slidesPerGroup: 3,
-
-	// 	// centeredSlides: true,
-	// 	loop: true,
-	// 	loopFillGroupWithBlank: true,
-	// 	touchRatio: 0.2,
-	// 	slideToClickedSlide: true,
-	// 	freeModeMomentum: true,
-	// 	navigation: {
-	// 		nextEl: '.swiper-button-next',
-	// 		prevEl: '.swiper-button-prev',
-	// 	},
-
-	// });
+	
 	// modal window
 
 
